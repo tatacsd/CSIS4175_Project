b96b666c8b1905e88bdf264d3cbd3154e4aa7e66
Thays Casado <thayscasado@hotmail.com>
2021-11-03 22:29:50 -0700

    main inflate fragments for home page account, deposit and transfer

--- app/src/main/java/com/group2/katching/HomeActivity.java
+++ app/src/main/java/com/group2/katching/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.group2.katching;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -13,22 +14,17 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        displayYourAccountFragment();
-        displayTransferFragment();
+        // Display all fragments
+        displayFragment(new YourAccountFragment(),R.id.accountFragment);
+        displayFragment(new DepositFragment(),R.id.depositFragment);
+        displayFragment(new TransferFragment(),R.id.transferFragment);
     }
 
-    public void displayYourAccountFragment() {
-        YourAccountFragment simpleFragment = YourAccountFragment.newInstance();
-
-        // TODO: Get the FragmentManager and start a transaction.
+    private void displayFragment(Fragment fragment, int containerViewId ) {
         FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragementTransaction = fragmentManager.beginTransaction();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(containerViewId, fragment).addToBackStack(null).commit();
     }
 
-    public void displayTransferFragment() {
 
-        // TODO: Get the FragmentManager and start a transaction.
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragementTransaction = fragmentManager.beginTransaction();
-    }
 }
