a46b20f7290029578e9ca9370dbb742120c649f9
ThreadSurfer <giladij@icloud.com>
2021-11-25 22:35:08 -0800

    set up signup activity. Works on db and forwards to home page

--- app/src/main/java/com/group2/katching/SignupActivity.java
+++ app/src/main/java/com/group2/katching/SignupActivity.java
@@ -2,14 +2,19 @@ package com.group2.katching;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
@@ -28,17 +33,20 @@ public class SignupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        Toast.makeText(SignupActivity.this, "test toast", Toast.LENGTH_SHORT).show();
+
         auth = FirebaseAuth.getInstance();
 
         signupInputEmail = (EditText) findViewById(R.id.txtUserEmail);
         signupInputPassword = (EditText) findViewById(R.id.password);
 
-        btnSignUp = (Button) findViewById(R.id.btnLogin);
+        btnSignUp = (Button) findViewById(R.id.btnSignup);
 
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 submitForm();
+                Toast.makeText(SignupActivity.this, "signed up", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -53,6 +61,25 @@ public class SignupActivity extends AppCompatActivity {
         if(!checkPassword()) {
             return;
         }
+        //setErrorEnabled
+
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+                        if(task.isSuccessful()) {
+                            startActivity(new Intent(SignupActivity.this, HomeActivity.class));
+                            finish();
+                        } else {
+                            Log.d(TAG, "Authentication failed." + task.getException());
+                        }
+                    }
+                });
+
+        Toast.makeText(getApplicationContext(),
+                "You are successfully registered!", Toast.LENGTH_SHORT).show();
+
 
     }
 
@@ -100,31 +127,23 @@ public class SignupActivity extends AppCompatActivity {
 //            signupInputPassword.setError(getString(R.string.err_msg_required));
 
             requestFocus(signupInputPassword);
-
             return false;
-
         }
         //Todo: Enter error message here
 //        signupInputLayoutPassword.setErrorEnabled(false);
-
         return true;
-
     }
 
 
 
     private static boolean isEmailValid(String email) {
-
         return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
-
     }
 
 
 
     private static boolean isPasswordValid(String password) {
-
         return (password.length() >= 6);
-
     }
 
 
@@ -132,21 +151,15 @@ public class SignupActivity extends AppCompatActivity {
     private void requestFocus(View view) {
 
         if (view.requestFocus()) {
-
             getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-
         }
 
     }
 
-
-
     @Override
 
     protected void onResume() {
-
         super.onResume();
-
 //        progressBar.setVisibility(View.GONE);
 
     }
