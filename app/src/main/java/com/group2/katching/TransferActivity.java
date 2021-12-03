c960d2337e9c9783c844eeb0f14b67a960388a00
Thays Casado <thayscasado@hotmail.com>
2021-12-03 14:40:05 -0800

    fixing keep sending money even without balance by send the user back to homeActivity

--- app/src/main/java/com/group2/katching/TransferActivity.java
+++ app/src/main/java/com/group2/katching/TransferActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -44,12 +46,12 @@ public class TransferActivity extends AppCompatActivity {
         TextView valueTransfer = findViewById(R.id.tv_value_transfer);
         EditText receivingUserEmail = findViewById(R.id.et_userTransfer);
 
-        //Retrieve data from transfer fragment intent
+        // Retrieve data from transfer fragment intent
         Bundle intentData = getIntent().getExtras();
         String userSendingKey = intentData.getString("key");
-        double[] userSendingBalance = new double[]{intentData.getDouble("balance")};
+        double[] userSendingBalance = new double[] { intentData.getDouble("balance") };
 
-        final Double[] transferValue = {0.00};
+        final Double[] transferValue = { 0.00 };
 
         btnDecAmount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,7 +60,8 @@ public class TransferActivity extends AppCompatActivity {
                     Toast.makeText(TransferActivity.this, "Negative values not accepted", Toast.LENGTH_SHORT).show();
                 }
                 if (transferValue[0] == 0) {
-                    Toast.makeText(TransferActivity.this, "The value for transference should be bigger than zero", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(TransferActivity.this, "The value for transference should be bigger than zero",
+                            Toast.LENGTH_SHORT).show();
                 } else {
                     transferValue[0] -= 0.50;
                     valueTransfer.setText(transferValue[0].toString());
@@ -77,7 +80,7 @@ public class TransferActivity extends AppCompatActivity {
         btnTransfer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //only validate if key has been properly sent in intent.
+                // only validate if key has been properly sent in intent.
 
                 // Checking user receiving email with database children
                 mFirebase = FirebaseDatabase.getInstance();
@@ -98,40 +101,58 @@ public class TransferActivity extends AppCompatActivity {
                             }
                         }
                         if (matchFound) {
-                            Log.v("found", "match for " + receivingUser[0].getEmail() + "(id: " + receivingUser[0].getDataBaseId() + ")" + " found.");
+                            Log.v("found", "match for " + receivingUser[0].getEmail() + "(id: "
+                                    + receivingUser[0].getDataBaseId() + ")" + " found.");
                             if (userSendingKey != null && receivingUser[0].getDataBaseId() != null) {
                                 // Get user sender amount
                                 Double amount = Double.valueOf(valueTransfer.getText().toString().replace("$ ", ""));
                                 Double userSendingBalanceValue = userSendingBalance[0];
-
-                                boolean ableToTransfer = userSendingBalance[0] >= 0 &&  userSendingBalance[0] >= amount;
+                                Log.v("Transfer:", "Testing the boolean BEFORE");
+                                boolean ableToTransfer = userSendingBalance[0] > 0 && userSendingBalance[0] >= amount;
+                                Log.v("Transfer:", "Testing the boolean BEFORE" + ableToTransfer);
                                 if (ableToTransfer) {
+                                    Log.v("Transfer:", "Testing the boolean INSIDE");
                                     // Start the transferring process
                                     Double newReceivingUserBalance = (receivingUser[0].getBalance() + amount);
                                     Double newSendingUserBalance = userSendingBalance[0] - amount;
                                     firebase.child(userSendingKey).child("balance").setValue(newSendingUserBalance);
-                                    firebase.child(receivingUser[0].getDataBaseId()).child("balance").setValue(newReceivingUserBalance);
-                                    //Show user successful deposit
-                                    Toast.makeText(TransferActivity.this, "transfer of " + amount + "to" + receivingUser[0].getEmail()+ " successful!"
-                                            , Toast.LENGTH_SHORT).show();
+                                    firebase.child(receivingUser[0].getDataBaseId()).child("balance")
+                                            .setValue(newReceivingUserBalance);
+                                    // Show user successful deposit
+                                    Toast.makeText(TransferActivity.this, "transfer of " + amount + "to"
+                                            + receivingUser[0].getEmail() + " successful!", Toast.LENGTH_SHORT).show();
 
-                                    Log.v("Transfer:", "transfer of " + amount + "to" + receivingUser[0].getEmail()+ " successful!"); //log success to console
+                                    Log.v("Transfer:", "transfer of " + amount + "to" + receivingUser[0].getEmail()
+                                            + " successful!"); // log success to console
+
                                     return;
+
                                 } else {
-                                    Toast.makeText(TransferActivity.this, "insufficient amount to transfer"
-                                            , Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(TransferActivity.this, "insufficient amount to transfer",
+                                            Toast.LENGTH_SHORT).show();
                                     Log.v("Transfer:", "insufficient amount to transfer");
                                 }
                             } else
                                 Log.v("not found", "no match found");
                         }
-
-
+                        return;
                     };
+
                     @Override
-                    public void onCancelled (@NonNull DatabaseError error){
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        return;
                     }
                 });
+
+                // set a time and return to main activity
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(TransferActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                    }
+                }, 1000);
+
             };
         });
     }
